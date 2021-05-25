2508f1d89b6701640a4478182f7c44bfbbe3b358
picoronkun <38022374+picoronkun@users.noreply.github.com>
2021-05-25 19:32:08 +0900

    [add]chat bot

--- chatbot-demo/src/App.jsx
+++ chatbot-demo/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import defaultDataset from './dataset';
 import '../src/assets/sytles/index.css';
 import '../src/assets/sytles/style.css';
-import {AnswersList} from './Components/index'; 
+import {AnswersList, Chats} from './Components/index'; 
+import FormDialog from './Components/Forms/FormDialog'; 
 
 export default class App extends React.Component {
   
@@ -11,26 +12,91 @@ export default class App extends React.Component {
         this.state = {
           answers: [],
           chats: [],
-          currentId: "init",
+         currentId: "init",
           dataset: defaultDataset,
-          opne: false
+          open: false
         }
+        this.selectAnswer = this.selectAnswer.bind(this)
+        this.handleClose = this.handleClose.bind(this)
+        this.handleClickOpen = this.handleClickOpen.bind(this)
       }
+
+      displayNextQuestion = (nextQuestionId) => {
+        const chats = this.state.chats
+        chats.push({
+          text: this.state.dataset[nextQuestionId].question,
+          type: "question"
+        }) 
+
+        this.setState({
+          answers: this.state.dataset[nextQuestionId].answers,
+          chats: chats,
+          currentId: nextQuestionId,
+        })
+         
+      }
+
+    selectAnswer = (selectedAnswer, nextQuestionId) => {
+       switch(true){
+         case (nextQuestionId === 'init'):
+           this.displayNextQuestion(nextQuestionId)
+           break;
+          case (/^https:*/.test(nextQuestionId)):
+            const a = document.createElement('a');
+            a.href = nextQuestionId;
+            a.target = '_blanck';
+            a.click();
+            break;
+          case (nextQuestionId === 'contact'):
+              this.handleClickOpen(); 
+              break;
+          default:
+            const chats = this.state.chats;
+            chats.push({
+              text: selectedAnswer,
+              type: 'answer'
+            })
+            
+            this.setState({
+              chats: chats
+            }) 
+            
+            setTimeout(() => this.displayNextQuestion(nextQuestionId), 1000)
+             break;
+
+       }
+    }
+
+    handleClickOpen = () => {
+      this.setState({open: true});
+    };
   
-  initAnswer = () =>{
-    const initDataset = this.state.dataset[this.state.currentId]
-    const initAnswers = initDataset[this.state.answers]
-    this.setState({
-      answers: initAnswers
-    })
+    handleClose = () => {
+        this.setState({open: false});
+    };
+
+  componentDidMount() {
+    const initAnswer = "";
+    this.selectAnswer(initAnswer, this.state.currentId)
+  }
+
+
+
+  componentDidUpdate() {
+    const scrollArea = document.getElementById('scroll-area')
+
+    if (scrollArea) {
+      scrollArea.scrollTop = scrollArea.scrollHeight
+    }
   }
     
   render() {
     return (
       <section className="c-section">
         <div className="c-box">
-          <AnswersList answers={this.state.answers} />
-
+          <Chats chats={this.state.chats} />
+          <AnswersList answers={this.state.answers} select={this.selectAnswer}/>
+          <FormDialog open={this.state.open} handleClose={this.handleClose}/>
         </div>
       </section>
     );
--- chatbot-demo/src/Components/AnswersList.jsx
+++ chatbot-demo/src/Components/AnswersList.jsx
@@ -4,10 +4,10 @@ import {Answer} from './index';
 const AnswersList = (props) => {
     return (
         <div className="c-grid__answer">
-            <Answer />
-            <Answer />
-            <Answer />
-            <Answer />
+            {props.answers.map((value, index) => {
+                return <Answer content={ value.content } nextId={value.nextId} key={index.toString()} select={props.select} />
+            })}
+
         </div>
     )
 }
